c9acdbea19a46e591f168474ec7910f5d456458b
UwUYukinekoUwU <weebcultured@gmail.com>
2023-10-15 23:04:06 +0200

    its getting late zzzz

--- main.py
+++ main.py
@@ -14,17 +14,16 @@ client_id = 1
 
 # The main behavior function for this server
 async def echo(websocket, path):
+    global client_id
     # don't let the exiled ones in
     client_ip = websocket.remote_address[0]
-
+    print(f"incoming ip: {websocket.remote_address} current id: {client_id}")
     if client_ip in banned_IPs.keys():
         if not check_if_unban(client_ip=client_ip):
-            disconnect(websocket=websocket)
             return
 
-    print("A client just connected")
+    print(f"A client just connected + {banned_IPs}")
     # Store a copy of the connected client
-    global client_id
     connected.append((client_id, websocket))
     client_id += 1
     # Handle incoming messages
@@ -51,28 +50,29 @@ async def echo(websocket, path):
     except websockets.exceptions.ConnectionClosed as e:
         print("A client just disconnected")
     finally:
-        disconnect(websocket)
+        await disconnect(websocket)
 
 
-def disconnect(websocket):
+async def disconnect(websocket):
     print("disconnected")
     for client in connected:
         if client[1] == websocket:
             connected.remove(client)
-
-def check_if_unban(client_ip):
-    if time.time() - banned_IPs[client_ip] > unban_time:
-        banned_IPs.__delitem__(client_ip)
-        return True
-
-    return False
+            await websocket.close()
 
 async def ban(websocket):
     print("banned")
     await websocket.send("SERVER_MESSAGE: Nuh uh.")
     banned_IPs[websocket.remote_address[0]] = time.time()
-    disconnect(websocket)
+    await disconnect(websocket)
 
+def check_if_unban(client_ip):
+    if time.time() - banned_IPs[client_ip] > unban_time:
+        banned_IPs.__delitem__(client_ip)
+        print(f"unbanned: {client_ip}")
+        return True
+
+    return False
 
 if __name__ == "__main__":
     start_server = websockets.serve(echo, "localhost", PORT)
